a98b421a21bc79f8bb784f10ca6a60ae5f13fc34
dophist <jerry.jiayu.du@gmail.com>
2021-03-09 12:17:20 +0800

    update jq install

--- utils/install_jq.sh
+++ utils/install_jq.sh
@@ -1,7 +1,12 @@
 
 if [ `uname -s` == 'Linux' ]; then
-  # this only works for Debian & Ubuntu for now
-  sudo apt-get install jq
+  if [ "`grep "NAME" /etc/os-release | grep Ubuntu`" != "" ] || [ "`grep "NAME" /etc/os-release | grep Debian`" != "" ]; then
+    apt-get install jq
+  elif [ "`grep NAME /etc/os-release | grep CentOS`" != "" ]
+    yum install jq
+  else
+    echo 'Unknown platform.'; exit -1
+  fi
 elif [ `uname -s` == 'Darwin' ]; then
   brew install jq
 fi
