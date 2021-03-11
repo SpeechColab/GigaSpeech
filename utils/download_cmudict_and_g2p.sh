46e7369f3c7e196015e8aa9848475718fe854546
王永庆 <wangyongqing3@xiaomi.com>
2021-03-11 11:18:01 +0800

    rm extra balnk

--- utils/download_cmudict_and_g2p.sh
+++ utils/download_cmudict_and_g2p.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # this script downloads cmudict and g2p models
 
-[ `uname -s` == 'Linux' ]  && ossbin=ossutil64
+[ `uname -s` == 'Linux' ] && ossbin=ossutil64
 [ `uname -s` == 'Darwin' ] && ossbin=ossutilmac64
 
-$ossbin -c SAFEBOX/aliyun_ossutil.cfg  cp ${GIGA_SPEECH_RELEASE_URL}/dict/cmudict.0.7a  $GIGA_SPEECH_LOCAL_ROOT/dict/cmudict.0.7a
-$ossbin -c SAFEBOX/aliyun_ossutil.cfg  cp -r ${GIGA_SPEECH_RELEASE_URL}/dict/g2p  $GIGA_SPEECH_LOCAL_ROOT/dict/
+$ossbin -c SAFEBOX/aliyun_ossutil.cfg cp ${GIGA_SPEECH_RELEASE_URL}/dict/cmudict.0.7a $GIGA_SPEECH_LOCAL_ROOT/dict/cmudict.0.7a
+$ossbin -c SAFEBOX/aliyun_ossutil.cfg cp -r ${GIGA_SPEECH_RELEASE_URL}/dict/g2p $GIGA_SPEECH_LOCAL_ROOT/dict/
