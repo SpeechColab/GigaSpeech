885fbf3d69c3a5a227b0fce0e03cf990445d3566
chenguoguo <chenguoguo06@gmail.com>
2021-03-24 19:57:37 +0000

    Added better error message handling to md5 checking

--- utils/check_audio_md5.sh
+++ utils/check_audio_md5.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # Copyright 2021  Jiayu DU
+#                 Seasalt AI, Inc (Author: Guoguo Chen)
+
+
+set -e
+set -o pipefail
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <gigaspeech-dataset-local-dir>"
@@ -12,6 +17,18 @@ fi
 
 gigaspeech_dataset_dir=$1
 
-utils/ls_md5.sh $gigaspeech_dataset_dir |\
-  md5sum -c - || exit 1
-echo "$0: Done md5 checking"
+failed=false
+utils/ls_md5.sh $gigaspeech_dataset_dir | (while read line; do
+  echo $line | md5sum -c --strict --quiet --status 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "$0: md5 check failed for: \"$line\""
+    failed=true
+  fi
+done
+
+if [ "$failed" = true ]; then
+  echo "$0: md5 checking failed, check the above logs."
+  exit 1
+fi) || exit 1
+
+echo "$0: Done md5 checking."
