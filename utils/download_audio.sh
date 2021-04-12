09b9a50968bec4b62cce55645b3ccb9785382069
chaisz19 <chaishuzhou@tju.edu.cn>
2021-04-12 17:50:29 +0800

    update
    When choosing XL, users will download ${GIGA_SPEECH_RELEASE_URL}/audio

--- utils/download_audio.sh
+++ utils/download_audio.sh
@@ -29,26 +29,28 @@ fi
 . ./env_vars.sh || exit 1;
 
 
-
 if [ -z "${GIGA_SPEECH_RELEASE_URL}" ]; then
   echo "ERROR: env variable GIGA_SPEECH_RELEASE_URL is empty(check env_vars.sh?)"
   exit 1
 fi
 
-
 gigaspeech_dataset_dir=$1
 GigaSpeech_json=$2
-
 
 [ `uname -s` == 'Linux' ] && ossbin=tools/downloader/ossutil64
 [ `uname -s` == 'Darwin' ] && ossbin=tools/downloader/ossutilmac64
 
-python3 utils/extract_subset.py \
-  $subset $GigaSpeech_json || exit ;
-for line in `cat log/subset_path`
-do
+if [ "$subset" == "XL" ];then 
   $ossbin -c SAFEBOX/aliyun_ossutil.cfg \
-    cp -ur ${GIGA_SPEECH_RELEASE_URL}/$line $gigaspeech_dataset_dir/$line || exit 1
-done
+    cp -ur ${GIGA_SPEECH_RELEASE_URL}/audio/ $gigaspeech_dataset_dir/audio/ || exit 1
+else
+  python3 utils/extract_subset.py \
+    $subset $GigaSpeech_json || exit ;
+  for line in `cat log/subset_path`
+  do
+    $ossbin -c SAFEBOX/aliyun_ossutil.cfg \
+      cp -ur ${GIGA_SPEECH_RELEASE_URL}/$line $gigaspeech_dataset_dir/$line || exit 1
+  done
+fi
 
 echo "$0: Done"
