505b034229be765f72a5f66a037e56f7afbd277b
chaisz19 <chaishuzhou@tju.edu.cn>
2021-05-07 11:39:45 +0800

    Update

--- utils/download_audio.sh
+++ utils/download_audio.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 # Copyright 2021  Jiayu Du
 #                 Seasalt AI, Inc (Author: Guoguo Chen)
-#                 Tsinghua University (Author: Shuzhou Chai) 
+#                 Tsinghua University (Author: Shuzhou Chai)
 
 set -e
 set -o pipefail
 
 
 subset=XL
+gigaSpeech_meta=
 
 . ./utils/parse_options.sh || exit 1;
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 [options] <gigaspeech-dataset-dir> <GigaSpeech-json> "
-  echo " e.g.: $0 --subset XL /disk1/audio_data/gigaspeech /disk1/GigaSpeech.json"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 [options] <gigaspeech-dataset-dir>"
+  echo " e.g.: $0 --subset XL --gigaSpeech_meta /disk1/audio_data/gigaspeech/GigaSpeech.json"
+  echo " /disk1/audio_data/gigaspeech "
   echo ""
   echo "This script downloads the entire GigaSpeech audio collection. We"
   echo "suggest having at least 600G of free space in local dir."
   echo "If audios are updated, you can just re-run this script for "
   echo "incremental downloading, downloader will only download updates"
-  echo "You may choose a specific subset to download, including {XS}, {S},"
-  echo "{M}, {L}, {XL}, {DEV}, {TEST} and the default subset is XL."
+  echo "You may choose a specific subset to download, including XS, S,"
+  echo "M, L, XL, DEV, TEST and the default subset is XL."
   echo "After downloading, you may run utils/check_audio_consistency.sh"
   echo "to make sure everything is consistent with official release."
   exit 1
@@ -35,19 +37,21 @@ if [ -z "${GIGA_SPEECH_RELEASE_URL}" ]; then
 fi
 
 gigaspeech_dataset_dir=$1
-GigaSpeech_json=$2
+
+if [ -z "$gigaSpeech_meta" ]; then
+  gigaSpeech_meta=$gigaspeech_dataset_dir/GigaSpeech.json
+fi
 
 [ `uname -s` == 'Linux' ] && ossbin=tools/downloader/ossutil64
 [ `uname -s` == 'Darwin' ] && ossbin=tools/downloader/ossutilmac64
 
-if [ "$subset" == "XL" ];then 
+if [ "$subset" == "XL" ]; then
   $ossbin -c SAFEBOX/aliyun_ossutil.cfg \
     cp -ur ${GIGA_SPEECH_RELEASE_URL}/audio/ $gigaspeech_dataset_dir/audio/ || exit 1
 else
   python3 utils/extract_subset.py \
-    $subset $GigaSpeech_json || exit ;
-  for line in `cat log/subset_path`
-  do
+    $subset $gigaSpeech_meta $gigaspeech_dataset_dir/subset_path_${subset} || exit ;
+  for line in `cat $gigaspeech_dataset_dir/subset_path_${subset}`; do
     $ossbin -c SAFEBOX/aliyun_ossutil.cfg \
       cp -ur ${GIGA_SPEECH_RELEASE_URL}/$line $gigaspeech_dataset_dir/$line || exit 1
   done
--- utils/extract_subset.py
+++ utils/extract_subset.py
@@ -5,44 +5,40 @@ import json
 
 
 def get_args():
-  parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(description="""
       This script is used to process raw json dataset of GigaSpeech
       to download selected subset.
       """)
-  parser.add_argument('subset', type=str, choices=['DEV', 'TEST', 'XL', 'L', 'M', 'S', 'XS'], default='XL', help="""Subset to download""")
-  parser.add_argument('input_json', help="""Input json file of Gigaspeech""")
-  args = parser.parse_args()
-  return args
+    parser.add_argument('subset', type=str, choices=[
+                        'DEV', 'TEST', 'XL', 'L', 'M', 'S', 'XS'], default='XL', help="""Subset to download""")
+    parser.add_argument('input_json', help="""Input json file of Gigaspeech""")
+    parser.add_argument(
+        'subset_path', help="""Output file with subset path of Gigaspeech""")
+    args = parser.parse_args()
+    return args
 
 
-def meta_analysis(input_json, subset):
-    subset_list=['{XS}', '{S}', '{M}', '{L}', '{XL}', '{TEST}', '{DEV}']
+def meta_analysis(input_json, subset, subset_path):
     if not os.path.exists(input_json):
         sys.exit(f'Failed to load input json file: {input_json}')
-    
-    if not os.path.exists('log'):
-        os.makedirs('log')
-    subset='{'+subset+'}'
-    print(subset)
-    if subset in subset_list:
-        with open(input_json, 'r') as injson:
-            json_data = json.load(injson)
-            if json_data['audios'] is not None:
-                with open(f'log/subset_path', 'w') as subset_path:
-                    for long_audio in json_data['audios']:
-                        subsets = long_audio['subsets']
-                        if subset in subsets:
-                            print(subsets)
-                            long_audio_path=long_audio['path']
-                            subset_path.write(f'{long_audio_path}\n')
-    else:
-        sys.exit(f'Subset should be XS, S, M, L, XL, DEV, TEST')
+
+    subset = '{' + subset + '}'
+    with open(input_json, 'r') as injson:
+        json_data = json.load(injson)
+        if json_data['audios'] is not None:
+            with open(subset_path, 'w') as f:
+                for long_audio in json_data['audios']:
+                    subsets = long_audio['subsets']
+                    if subset in subsets:
+                        long_audio_path = long_audio['path']
+                        f.write(f'{long_audio_path}\n')
+
 
 def main():
-  args = get_args()
+    args = get_args()
 
-  meta_analysis(args.input_json, args.subset)
+    meta_analysis(args.input_json, args.subset, args.subset_path)
 
 
 if __name__ == '__main__':
-  main()
+    main()
