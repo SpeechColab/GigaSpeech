18bd5bcc48d4117135f5c45f480e18a50a7350fb
chenguoguo <chenguoguo06@gmail.com>
2021-07-23 21:23:40 -0700

    Edits to meta md5 check

--- utils/check_meta_md5.sh
+++ utils/check_meta_md5.sh
@@ -11,13 +11,19 @@ if [ $# -ne 1 ]; then
   echo " e.g.: $0 /disk1/audio_data/gigaspeech"
   echo ""
   echo "This script tries to detect errors in the downloaded meta data "
-  echo "by checking local GigaSpeech.json MD5 with misc/meta_versions.txt"
+  echo "by checking the md5 value. You can find the expected md5 value in"
+  echo "misc/meta_versions.txt."
   exit 1
 fi
 
 gigaspeech_dataset_dir=$1
 
-verified=false
+if [ ! -f $gigaspeech_dataset_dir/GigaSpeech.json ]; then
+  echo "$0: Metadata $gigaspeech_dataset_dir/GigaSpeech.json does not exist."
+  exit
+fi
+
+verified="false"
 local_version=$(utils/meta_version.sh $gigaspeech_dataset_dir)
 if [[ `uname -s` == "Linux" ]]; then
   if ! which md5sum >/dev/null; then
@@ -32,17 +38,17 @@ elif [[ `uname -s` == "Darwin" ]]; then
   fi
   local_md5=$(md5 -r $gigaspeech_dataset_dir/GigaSpeech.json | awk '{print $1}')
 else
-  echo "$0: $0 only supports Linux and Mac OS"
+  echo "$0: only supports Linux and Mac OS"
   exit 1
 fi
 
 grep -v '^#' misc/meta_versions.txt | (while read line; do
   version=$(echo $line | awk '{print $1}')
   md5=$(echo $line | awk '{print $2}')
-  if [ "$local_version" == "$version" ]; then
-    if [ "$local_md5" == "$md5" ]; then
+  if [[ "$local_version" == "$version" ]]; then
+    if [[ "$local_md5" == "$md5" ]]; then
       echo "$0: Successfully verified meta version:$version, md5:$md5"
-      verified=true
+      verified="true"
     else
       echo "$0: ERROR, $local_version expects md5=$md5, got $local_md5"
       exit 1;
@@ -50,7 +56,7 @@ grep -v '^#' misc/meta_versions.txt | (while read line; do
   fi
 done
 
-if [ "$verified" = false ]; then
+if [[ "$verified" == "false" ]]; then
   echo "$0: md5 verification failed for unknown version $local_version"
   exit 1
 fi) || exit 1;
