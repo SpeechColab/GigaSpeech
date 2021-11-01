cdf5a53b423e383d65d99f65c4dab03948e5ab49
Junbo Zhang <zhangjunbo1@xiaomi.com>
2021-11-01 14:52:30 +0800

    bug fix

--- utils/download_gigaspeech.sh
+++ utils/download_gigaspeech.sh
@@ -50,6 +50,7 @@ fi
 PASSWORD=`cat SAFEBOX/password 2>/dev/null`
 if [ -z "$PASSWORD" ]; then
   echo "$0: Error, SAFEBOX/password is empty."
+  exit 1;
 fi
 PASSWORD_MD5=`echo $PASSWORD | md5sum | cut -d ' ' -f 1`
 if [[ $PASSWORD_MD5 != "dfbf0cde1a3ce23749d8d81e492741b8" ]]; then
